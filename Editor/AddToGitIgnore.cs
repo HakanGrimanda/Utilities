045dd2c639660c15c00efa1eb24a076ecc51b02d
Hakan Yuksel <hakan@grimanda.com>
2022-10-07 10:18:45 +0300

    Remove from gitignore is added

--- Editor/AddToGitIgnore.cs
+++ Editor/AddToGitIgnore.cs
@@ -1,8 +1,10 @@
 using UnityEditor;
 using System.IO;
+using System.Diagnostics;
+
 public static class AddToGitIgnore
 {
-    [MenuItem("Assets/AddToGitIgnore")]
+    [MenuItem("Assets/Add To GitIgnore")]
     private static void AddToGitIgnoreFile()
     {
         string path = "Assets/.gitignore";
@@ -10,7 +12,7 @@ public static class AddToGitIgnore
         writer.Close();
 
         StreamReader reader = new StreamReader(path, true);
-        string previous=reader.ReadToEnd();
+        string previous = reader.ReadToEnd();
         reader.Close();
 
         writer = new StreamWriter(path, true);
@@ -24,6 +26,33 @@ public static class AddToGitIgnore
             }
         }
 
+        writer.Close();
+    }
+
+    [MenuItem("Assets/Remove From GitIgnore")]
+    private static void RemoveFromGitIgnoreFile()
+    {
+        string path = "Assets/.gitignore";
+        StreamWriter writer = new StreamWriter(path, true);
+        writer.Close();
+
+        StreamReader reader = new StreamReader(path, true);
+        string previous = reader.ReadToEnd();
+        reader.Close();
+
+        writer = new StreamWriter(path, false);
+
+        foreach (UnityEngine.Object item in Selection.objects)
+        {
+            if (previous.Contains(item.name + "\r\n"))
+            {
+                Debug.Write(item.name);
+
+                previous = previous.Replace(item.name + "\r\n", "");
+                previous = previous.Replace(item.name + ".meta\r\n", "");
+            }
+        }
+        writer.Write(previous);
         writer.Close();
     }
 }
