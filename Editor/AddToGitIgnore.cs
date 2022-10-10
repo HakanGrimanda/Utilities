1c68e7fcf6a3112710299e1610d5b8847406177a
Hakan Yuksel <hakan@grimanda.com>
2022-10-10 11:56:40 +0300

    Fixed the file name contains another files name bug.

--- Editor/AddToGitIgnore.cs
+++ Editor/AddToGitIgnore.cs
@@ -1,58 +1,65 @@
 using UnityEditor;
 using System.IO;
-using System.Diagnostics;
+using UnityEngine;
+using System.Collections.Generic;
 
 public static class AddToGitIgnore
 {
-    [MenuItem("Assets/Add To GitIgnore")]
-    private static void AddToGitIgnoreFile()
+    private static List<string> DoesListContainsLine(List<string> lines, string lineToBeLookedFor, bool shouldAdd)
+    {
+        List<string> newLines = new();
+        foreach (string line in lines)
+        {
+            if (lineToBeLookedFor != line)
+            {
+                newLines.Add(line);
+            }
+        }
+
+        if (shouldAdd)
+        {
+            newLines.Add(lineToBeLookedFor);
+        }
+        return newLines;
+    }
+
+    private static void AddRemoveFromGitIgnore(bool shouldAdd)
     {
         string path = "Assets/.gitignore";
         StreamWriter writer = new StreamWriter(path, true);
         writer.Close();
 
-        StreamReader reader = new StreamReader(path, true);
-        string previous = reader.ReadToEnd();
+        StreamReader reader = new(path, true);
+        List<string> previous = new();
+        string line;
+        while ((line = reader.ReadLine()) != null)
+        {
+            previous.Add(line);
+        }
         reader.Close();
-
-        writer = new StreamWriter(path, true);
 
         foreach (UnityEngine.Object item in Selection.objects)
         {
-            if (!previous.Contains(item.name))
-            {
-                writer.WriteLine(item.name);
-                writer.WriteLine(item.name + ".meta");
-            }
+            previous = DoesListContainsLine(previous, item.name, shouldAdd);
+            previous = DoesListContainsLine(previous, item.name + ".meta", shouldAdd);
         }
-
+        writer = new StreamWriter(path, false);
+        foreach (string _line in previous)
+        {
+            writer.WriteLine(_line);
+        }
         writer.Close();
+    }
+
+    [MenuItem("Assets/Add To GitIgnore")]
+    private static void AddToGitIgnoreFile()
+    {
+        AddRemoveFromGitIgnore(true);
     }
 
     [MenuItem("Assets/Remove From GitIgnore")]
     private static void RemoveFromGitIgnoreFile()
     {
-        string path = "Assets/.gitignore";
-        StreamWriter writer = new StreamWriter(path, true);
-        writer.Close();
-
-        StreamReader reader = new StreamReader(path, true);
-        string previous = reader.ReadToEnd();
-        reader.Close();
-
-        writer = new StreamWriter(path, false);
-
-        foreach (UnityEngine.Object item in Selection.objects)
-        {
-            if (previous.Contains(item.name + "\r\n"))
-            {
-                Debug.Write(item.name);
-
-                previous = previous.Replace(item.name + "\r\n", "");
-                previous = previous.Replace(item.name + ".meta\r\n", "");
-            }
-        }
-        writer.Write(previous);
-        writer.Close();
+        AddRemoveFromGitIgnore(false);
     }
 }
